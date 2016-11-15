5c9e58a1c08ab17de52a62aba47ceb3749f74141
Andreas Korb <Andreas.D.Korb@gmail.com>
2016-11-15 15:07:18 +0100

    Added message box after errors

--- SteamShutdown/Program.cs
+++ SteamShutdown/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SteamShutdown
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Main());
-		}
-	}
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "SteamShutdown_Log.txt");
+            File.WriteAllText(path, e.ExceptionObject.ToString());
+
+            MessageBox.Show("Please send me the log file on GitHub or via E-Mail (Andreas.D.Korb@gmail.com)" + Environment.NewLine + path, "An Error occured");
+        }
+    }
 }
