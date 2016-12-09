ffcac790835816d0694b3657e619846471fb2817
Andreas Korb <Andreas.D.Korb@gmail.com>
2016-12-09 16:41:43 +0100

    Added unit tests

---
+++ SteamShutdown.Tests/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SteamShutdown.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void AppInfoTests()
+        {
+            AppInfo ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_213670.acf");
+            Assert.AreEqual(ai.ID, 213670);
+            Assert.AreEqual(ai.Name, "South Park™: The Stick of Truth™");
+
+            ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_252490.acf");
+            Assert.AreEqual(ai, null);
+
+            ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_286080.acf");
+            Assert.AreEqual(ai.ID, 286080);
+            Assert.AreEqual(ai.Name, "Thinking with Time Machine");
+
+            ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_403640.acf");
+            Assert.AreEqual(ai.ID, 403640);
+            Assert.AreEqual(ai.Name, "Dishonored 2");
+        }
+    }
+}
