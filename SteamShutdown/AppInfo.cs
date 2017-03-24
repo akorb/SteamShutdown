39c448dfb83506b17ffcfde1dc4707ad5ec5a9b0
akorb <andreas.d.korb@gmail.com>
2017-03-24 17:17:37 +0100

    Detects automatically queued update now

--- SteamShutdown/AppInfo.cs
+++ SteamShutdown/AppInfo.cs
@@ -6,14 +6,25 @@
         public string Name { get; set; }
         public int State { get; set; }
 
+        /// <summary>
+        /// Returns a value indicating whether the game is being downloaded. Includes games in queue for download.
+        /// </summary>
         public bool IsDownloading
         {
             get { return CheckDownloading(State); }
         }
 
+        /// <summary>
+        /// Returns a value indicating whether the game is being downloaded. Includes games in queue for download.
+        /// </summary>
+        /// <param name="appState"></param>
+        /// <returns></returns>
         public static bool CheckDownloading(int appState)
         {
-            return appState == 1026 || appState == 1042 || appState == 1062 || appState == 1030;
+            // 6: In queue for update
+            // 1024: download running
+            // 1026: In queue
+            return appState == 6 || appState == 1026 || appState == 1042 || appState == 1062 || appState == 1030;
         }
 
         public override string ToString()
