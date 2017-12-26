24e02b8c8ff3388390e71d86be8114047ee6aaf7
akorb <andreas.d.korb@gmail.com>
2017-12-26 23:39:57 +0100

    Renamed file

---
+++ SteamShutdown/Modes/HibernateMode.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace SteamShutdown.Modes
+{
+    public class HibernationMode : Mode
+    {
+        public override string Name { get; protected set; } = "Hibernate";
+
+        public override void Execute()
+        {
+            Application.SetSuspendState(PowerState.Hibernate, false, false);
+        }
+    }
+}
