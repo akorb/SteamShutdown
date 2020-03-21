8718578e82f20eb0756d71d6b5a3bd8e1c085a59
akorb <andreas.d.korb@gmail.com>
2020-03-21 13:33:12 +0100

    Update version

--- SteamShutdown.Tests/Properties/AssemblyInfo.cs
+++ SteamShutdown.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AppInfoTests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
