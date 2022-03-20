ba23bc7e7fda24916103fd9b5fa2dc0aafad2361
Andreas <andreas.d.korb@gmail.com>
2022-03-20 21:06:38 +0100

    Also check if State property parsed correctly Improve paths and filenames

--- SteamShutdown.Tests/AcfParsingTests.cs
+++ SteamShutdown.Tests/AcfParsingTests.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace SteamShutdown.Tests
 {
@@ -9,20 +8,23 @@ namespace SteamShutdown.Tests
         [TestMethod]
         public void AppInfoTests()
         {
-            App ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_213670.acf");
+            App ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_213670.acf");
             Assert.AreEqual(ai.ID, 213670);
             Assert.AreEqual(ai.Name, "South Park™: The Stick of Truth™");
+            Assert.AreEqual(ai.State, 4);
 
-            ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_252490.acf");
+            ai = Steam.FileToAppInfo("AcfFiles\\corrupted.acf");
             Assert.AreEqual(ai, null);
 
-            ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_286080.acf");
+            ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_286080.acf");
             Assert.AreEqual(ai.ID, 286080);
             Assert.AreEqual(ai.Name, "Thinking with Time Machine");
+            Assert.AreEqual(ai.State, 4);
 
-            ai = Steam.FileToAppInfo(AppDomain.CurrentDomain.BaseDirectory + @"\..\..\..\..\TestFiles\appmanifest_403640.acf");
+            ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_403640.acf");
             Assert.AreEqual(ai.ID, 403640);
             Assert.AreEqual(ai.Name, "Dishonored 2");
+            Assert.AreEqual(ai.State, 4);
         }
     }
 }
