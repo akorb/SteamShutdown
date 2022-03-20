b50e0690ded39d0d16ea18323cf665b5ef2c3ea7
Andreas <andreas.d.korb@gmail.com>
2022-03-20 21:14:25 +0100

    Split acf tests and add test for empty acf file

--- SteamShutdown.Tests/AcfParsingTests.cs
+++ SteamShutdown.Tests/AcfParsingTests.cs
@@ -6,25 +6,43 @@ namespace SteamShutdown.Tests
     public class AcfParsingTests
     {
         [TestMethod]
-        public void AppInfoTests()
+        public void Valid1()
         {
             App ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_213670.acf");
             Assert.AreEqual(213670, ai.ID);
             Assert.AreEqual("South Park™: The Stick of Truth™", ai.Name);
             Assert.AreEqual(4, ai.State);
+        }
 
-            ai = Steam.FileToAppInfo("AcfFiles\\corrupted.acf");
-            Assert.AreEqual(null, ai);
-
-            ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_286080.acf");
+        public void Valid2()
+        {
+            App ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_286080.acf");
             Assert.AreEqual(286080, ai.ID);
             Assert.AreEqual("Thinking with Time Machine", ai.Name);
             Assert.AreEqual(4, ai.State);
+        }
 
-            ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_403640.acf");
+        [TestMethod]
+        public void Valid3()
+        {
+            App ai = Steam.FileToAppInfo("AcfFiles\\appmanifest_403640.acf");
             Assert.AreEqual(403640, ai.ID);
             Assert.AreEqual("Dishonored 2", ai.Name);
             Assert.AreEqual(4, ai.State);
+        }
+
+        [TestMethod]
+        public void CorruptNulls()
+        {
+            App ai = Steam.FileToAppInfo("AcfFiles\\corrupted.acf");
+            Assert.AreEqual(null, ai);
+        }
+
+        [TestMethod]
+        public void CorruptEmpty()
+        {
+            App ai = Steam.FileToAppInfo("AcfFiles\\empty.acf");
+            Assert.AreEqual(null, ai);
         }
     }
 }
