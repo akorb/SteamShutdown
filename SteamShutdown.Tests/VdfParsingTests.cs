f386a630097adda2b7b52e4873b34fc5947b35c0
Andreas <andreas.d.korb@gmail.com>
2022-03-20 21:53:35 +0100

    Add vdf tests

---
+++ SteamShutdown.Tests/VdfParsingTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static SteamShutdown.Steam;
+
+namespace AppInfoTests
+{
+    [TestClass]
+    public class VdfParsingTests
+    {
+        [TestMethod]
+        public void OldFormat()
+        {
+            string[] libraryPaths = InstallationPathsFromVdf("VdfFiles\\old_format.vdf");
+            Assert.AreEqual(2, libraryPaths.Length);
+            Assert.AreEqual("E:\\Gry\\Steam\\SteamApps", libraryPaths[0]);
+            Assert.AreEqual("F:\\Gry\\Steam\\SteamApps", libraryPaths[1]);
+        }
+
+        [TestMethod]
+        public void NewFormatWithoutMounted()
+        {
+            string[] libraryPaths = InstallationPathsFromVdf("VdfFiles\\new_format_without_mounted.vdf");
+            Assert.AreEqual(2, libraryPaths.Length);
+            Assert.AreEqual("D:\\Games\\SteamApps", libraryPaths[0]);
+            Assert.AreEqual("F:\\SteamLibrary\\SteamApps", libraryPaths[1]);
+        }
+
+        [TestMethod]
+        public void NewFormatWithMounted()
+        {
+            string[] libraryPaths = InstallationPathsFromVdf("VdfFiles\\new_format_with_mounted.vdf");
+            Assert.AreEqual(1, libraryPaths.Length);
+            Assert.AreEqual("E:\\SteamLibrary\\SteamApps", libraryPaths[0]);
+        }
+    }
+}
