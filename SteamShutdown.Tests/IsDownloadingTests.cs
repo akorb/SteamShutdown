c00add194ea324665749f2edd80f1e2a7f8f8259
Andreas <andreas.d.korb@gmail.com>
2022-03-20 20:58:06 +0100

    Add tests for download detection

---
+++ SteamShutdown.Tests/IsDownloadingTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static SteamShutdown.App;
+
+namespace SteamShutdown.Tests
+{
+    [TestClass]
+    public class IsDownloadingTests
+    {
+        [TestMethod]
+        public void TestDownloadingStates()
+        {
+            int[] downloadingStates =
+            {
+                6,
+                1026,
+                1030,
+                1042,
+                1044,
+                1062,
+            };
+
+            foreach (int state in downloadingStates)
+            {
+                Assert.IsTrue(CheckDownloading(state), $"State {state} was erroneously detected as not downloading.");
+            }
+        }
+
+        [TestMethod]
+        public void TestNotDownloadingStates()
+        {
+            int[] downloadingStates =
+            {
+                4,
+                70,
+            };
+
+            foreach (int state in downloadingStates)
+            {
+                Assert.IsFalse(CheckDownloading(state), $"State {state} was erroneously detected as downloading.");
+            }
+        }
+    }
+}
